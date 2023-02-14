91b31419d9a334302cb933596579c7b6f99c8b5e
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2023-02-14 16:39:48 +0500

    3 methods have been refactored

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -6,21 +6,21 @@ import java.util.List;
 public class Generics {
 
     //TODO: Refactor Method-1
-    public  List<List<String>> boxingMethod(String name) {
+    public List<List<String>> boxingMethod(String name) {
         List<String> firstList = new ArrayList<>();
         firstList.add(name);
         List<List<String>> secondList = new ArrayList<>();
         secondList.add(firstList);
-        return  secondList;
+        return secondList;
     }
 
     //TODO: Refactor Method-2
-    public <T>Object genericMethod(Object data) {
+    public <T> Object genericMethod(Object data) {
         return data;
     }
 
     //TODO: Refactor Method-3
-    public <T> void cloneMethod( List<? super T> consumer, List<? extends T> producer) {
+    public <T> void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
 
